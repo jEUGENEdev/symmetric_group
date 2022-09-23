e698ac6a3033fd9e101efa486c346ea04f1f1f2b
Eugene <genyamateyuk@gmail.com>
2022-09-23 19:39:11 +0300

    added symmetric group in array builder Group.java

--- src/main/java/org/math/calc/Group.java
+++ src/main/java/org/math/calc/Group.java
@@ -1,5 +1,9 @@
 package org.math.calc;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Group {
@@ -9,5 +13,39 @@ public class Group {
 
     public Group(String[] groups) {
         this.groups = groups;
+        System.out.println(equalityOfSets());
+    }
+
+    private boolean equalityOfSets() {
+        Set<Integer> set = new HashSet<>();
+        for (String group : groups) {
+            String[] groupOne = group
+                    .replace(")(", "*")
+                    .replace("(", "")
+                    .replace(")", "")
+                    .split("\\*");
+            buildTable(groupOne);
+        }
+        return true;
+    }
+
+    private int[][] buildTable(String[] cycleNoteGroup) {
+        int maxEl = 0;
+        for (String cycle : cycleNoteGroup)
+            for (String number : cycle.split("\\s")) {
+                int cur = Integer.parseInt(number);
+                if (cur > maxEl)
+                    maxEl = cur;
+            }
+        int[][] group = new int[2][maxEl];
+        for (int i = 0; i < maxEl; i++)
+            group[0][i] = i + 1;
+        for (String cycle : cycleNoteGroup) {
+            List<Integer> numbers = Arrays.stream(cycle.split("\\s")).map(Integer::parseInt).toList();
+            for (int i = 0; i < numbers.size() - 1; i++)
+                group[1][numbers.get(i) - 1] = numbers.get(i + 1);
+            group[1][numbers.get(numbers.size() - 1) - 1] = numbers.get(0);
+        }
+        return group;
     }
 }
